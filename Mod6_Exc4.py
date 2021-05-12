6f07a165230b2b8e7a4929510555be884f723c3d
jmveronez <jmveronez234@gmail.com>
2021-05-12 19:19:39 -0300

    Resolução do 4º exercício do 6º módulo

---
+++ Mod6_Exc4.py
@@ -0,0 +1,4 @@
+#4.	Faça um programa que calcule o salário de um colaborador na empresa XYZ. 
+# O salário é pago conforme a quantidade de horas trabalhadas. 
+# Quando um funcionário trabalha mais de 40 horas
+#  ele recebe um adicional de 1.5 nas horas extras trabalhadas.
