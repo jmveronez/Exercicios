c19e752dda8b8c2fdc6c35b73edf66d1cd8e4c5e
jmveronez <jmveronez234@gmail.com>
2021-05-12 19:35:24 -0300

    Resolução do 4º exercício do 6º módulo da Blue

--- Mod6_Exc4.py
+++ Mod6_Exc4.py
@@ -2,3 +2,19 @@
 # O salário é pago conforme a quantidade de horas trabalhadas. 
 # Quando um funcionário trabalha mais de 40 horas
 #  ele recebe um adicional de 1.5 nas horas extras trabalhadas.
+
+def pagamento(horas, valor):
+    horas_trabalhadas = float(horas)
+    valor_horas = float(valor)
+    if horas_trabalhadas <= 40:
+        salario = horas*valor
+    else:
+        horas_extras = horas_trabalhadas - 40
+        salario = 40*valor_horas+(horas_extras*(1.5*valor_horas))
+    return salario
+
+str_horas = int(input("Quantas horas foram trabalhadas no mês? "))
+str_taxa = int(input("Qual a taxa de valor por hora trabalhada? "))
+
+salario_total = pagamento(str_horas, str_taxa)
+print("Você irá receber R$",salario_total)
