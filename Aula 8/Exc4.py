d4e54913df407054e4f4af6198d8879acff7d020
jmveronez <jmveronez234@gmail.com>
2021-05-13 21:43:16 -0300

    Resolução do 4º exercício da 8ª aula

--- Aula 8/Exc4.py
+++ Aula 8/Exc4.py
@@ -1,2 +1,20 @@
 #4.	Dada uma string com uma frase informada pelo usuário 
 #(incluindo espaços em branco), conte quantas vezes aparece uma vogal.
+
+def conta_vogal(frase):
+    vogais = 0
+    for letra in frase:
+        if letra.upper() == "A":
+            vogais += 1
+        elif letra.upper() == "E":
+            vogais += 1
+        elif letra.upper() == "I":
+            vogais += 1
+        elif letra.upper() == "O":
+            vogais += 1
+        elif letra.upper() == "U":
+            vogais += 1
+    return vogais
+
+frase_str = input("Digite uma palavra ou frase para contarmos as vogais: ")
+print("A palavra/frase",frase_str,"possui",conta_vogal(frase_str),"vogais")
