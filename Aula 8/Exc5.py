914ed7ea8880b220c414f216d410c83991299885
jmveronez <jmveronez234@gmail.com>
2021-05-13 22:17:45 -0300

    Resolução do 5º exercício

--- Aula 8/Exc5.py
+++ Aula 8/Exc5.py
@@ -1,2 +1,13 @@
 #5.	Desenvolva um código que pergunte um número n para o usuário e exiba todos seus divisores. 
 # Caso seja um número primo, o programa ainda deve informar que se trata de um número primo! 
+
+n = int(input("Digite um número inteiro:"))
+print("Os divisores de ", n,"são: ")
+contador = 0
+for i in range(n,0,-1):
+    if(n % i == 0):
+        print(i)
+        contador += 1
+    
+if contador == 2:
+    print(n,"é um número primo")
