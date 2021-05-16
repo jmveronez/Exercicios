41bb2428f90b2497dfc38042661f4aa7a369ec74
jmveronez <jmveronez234@gmail.com>
2021-05-15 21:01:51 -0300

    Resolução do Desafio

--- Aula 6/Desafio1.py
+++ Aula 6/Desafio1.py
@@ -2,3 +2,40 @@
 #D de mesPorExtenso de AAAA. Opcionalmente, valide a data e retorne NULL caso a data seja inválida.
 #Considere que Fevereiro tem 28 dias e que a cada 4 anos temos ano bisexto, sendo que nesses casos
 #Fevereiro terá 29 dias.
+
+def Escreve_Data(dia,mes,ano):
+    if dia > 0 and dia <= 31 and ano > 0:
+        if mes == 1:
+            print(dia,"de Janeiro de",ano)
+        elif mes == 2:
+            print(dia,"de Fevereiro de",ano)
+        elif mes == 3:
+            print(dia,"de Março de",ano)
+        elif mes == 4:
+            print(dia,"de Abril de",ano)
+        elif mes == 5:
+            print(dia,"de Maio de",ano)
+        elif mes == 6:
+            print(dia,"de Junho de",ano)
+        elif mes == 7:
+            print(dia,"de Julho de",ano)
+        elif mes == 8:
+            print(dia,"de Agosto de",ano)
+        elif mes == 9:
+            print(dia,"de Setembro de",ano)
+        elif mes == 10:
+            print(dia,"de Outubro de",ano)
+        elif mes == 11:
+            print(dia,"de Novembro de",ano)
+        elif mes == 12:
+            print(dia,"de Dezembro de",ano)
+        else:
+            print("NULL")
+    else:
+        print("NULL")
+
+str_dia = int(input("Digite o dia: "))
+str_mes = int(input("Digite o mês: "))
+str_ano = int(input("Digite o ano: "))
+
+Escreve_Data(str_dia,str_mes,str_ano)
